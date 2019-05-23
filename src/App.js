dc8d924ff577a1059df9e4ae17fd8b1c3b8133a9
xmastree <huseyinozan.honamlioglu@gunsel.com.tr>
2019-05-23 16:35:20 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ozan <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          we are in docker :)
         </p>
         <a
           className="App-link"
@@ -16,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with me bitches
         </a>
       </header>
     </div>
